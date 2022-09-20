e4175eee5dd85f652cb4319baf8330ec0ca19838
Shawn Gregory <shawn.gregory416@outlook.com>
2022-09-19 18:46:31 -0700

    first commit

---
+++ dice.js
@@ -0,0 +1,8 @@
+
+//function for rolling d6 | 1d6
+function dice(numOfDice) {
+    Math.floor(Math.random * numOfDice) +1;
+}
+
+//function to roll multiple dice | xD6
+function numDiceAmount( numOfTimes , numOfDice)
